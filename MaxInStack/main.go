dd127c1084f779ccda516bc86d168c3d9ae53ece
Egor Mikhailov <egor11022004@gmail.com>
2023-09-05 21:47:26 +0300

    Max of O(1)

--- MaxInStack/main.go
+++ MaxInStack/main.go
@@ -8,17 +8,22 @@ import (
 type Stack struct {
 	arr []int
 	i   int
+	max int
 }
 
 func NewStack() *Stack {
 	return &Stack{
 		arr: make([]int, 0),
+		max: math.MinInt,
 		i:   -1,
 	}
 }
 
 func (s *Stack) push(val int) {
 	s.arr = append(s.arr, val)
+	if val > s.max {
+		s.max = val
+	}
 	s.i++
 }
 
@@ -30,19 +35,25 @@ func (s *Stack) pop() int {
 	res := s.arr[s.i]
 	s.arr = s.arr[:s.i]
 	s.i--
+	s.setMax()
 	return res
 }
 
-func (s *Stack) Max() int {
-	indCopy := s.i
-	max := math.MinInt
-	for indCopy >= 0 {
-		if s.arr[indCopy] > max {
-			max = s.arr[indCopy]
+func (s *Stack) setMax() {
+	index := s.i
+	for index >= 0 {
+		if s.arr[s.i] > s.max {
+			s.max = s.arr[s.i]
 		}
-		indCopy--
+		index--
 	}
-	return max
+}
+
+func (s *Stack) Max() int {
+	if s.i >= 0 {
+		return s.max
+	}
+	return math.MinInt
 }
 
 func main() {
